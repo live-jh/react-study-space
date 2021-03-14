7b06f89f79b6006c1ca72a052fcd21547baf93ca
live-jh <livejh7129@gmail.com>
2021-03-15 06:59:58 +0900

    ➕ add pure_function and currying

--- ask-react-project/src/es6_example_2.js
+++ ask-react-project/src/es6_example_2.js
@@ -21,3 +21,33 @@ const mysum = (a, b) => a + b;
 
 const return_fn = base_10(mysum); //인자의 mysum은 mysum 함수가 아니라 base_10 내부의 wrap함수 1, 2는 wrap의 인자
 console.log(return_fn(1, 2))
+
+
+let person1 = {
+    name: "steve",
+    age: 1,
+    is_checed: false
+}
+
+const pure_func = (person) => {
+    return {
+        ...person,
+        is_checed: true
+    }
+}
+
+
+console.log(pure_func(person1))
+
+const numbers = [3, 5, 123, 2]
+const number = numbers.reduce((acc, n) => acc + n, 0); // accumulate 의 초기값 0
+const even_numbers = numbers.filter(i => i % 2 == 0);
+console.log(even_numbers)
+console.log(number)
+
+
+const userPost = username => message => {
+		console.log(`${username} - ${message}`);
+}
+const post = userPost("livejh")
+post("Hello")
