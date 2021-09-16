645e89a8ebefd6296242bab74b05473e4f6f4130
live-jh <livejh7129@gmail.com>
2021-09-17 07:03:02 +0900

    update go pipe!

--- inflearn_func_js/js/fx.js
+++ inflearn_func_js/js/fx.js
@@ -3,9 +3,19 @@ const log = console.log;
 const curry = f =>
   (a, ..._) => _.length ? f(a, ..._) : (..._) => f(a, ..._);
 
-const go = (...args) => reduce((a, f) => f(a), args);
+// reduce (합치기), a는 0, f는 함수, reduce엔 앞에 연산, 뒤에 함수를 넣는다
+const go = (...args) => reduce((a, f) => f(a), args); // (f(a)[연산], args[iter])
 
-const pipe = (f, ...fs) => (...as) => go(f(...as), ...fs);
+// 내부에 전달받은 여러 함수를 이용해 결과값(함수)을 축약, 함수 리턴
+// as 인자를 받아 go 함수를 호출하는데 인자 받은 함수를 그대로 넘겨준다
+// as 인자는 pipe의 첫번째 인자이다
+const pipe = (f, ...funcs) => (...as) => {
+      log(...as) // func의 파라미터
+      log(f) // pipe의 첫번째 인자
+      log(funcs) // pipe의 2,3번째 인자
+      return go(f(...as), ...funcs)
+};
+
 
 const take = curry((l, iter) => {
   let res = [];
